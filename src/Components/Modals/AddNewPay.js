33374696685fbbb3876f1a99df5107c6bb14322a
CarlosADLRT <cadelosreyest@gmail.com>
2019-05-25 19:40:42 -0500

    make functional add new pay compoennt

--- src/Components/Modals/AddNewPay.js
+++ src/Components/Modals/AddNewPay.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Formik, Form, Field } from 'formik';
@@ -7,78 +7,67 @@ import { format } from 'date-fns';
 import { bindActionCreators } from 'redux';
 import { initAddPay } from '../../Redux/Actions/ActionsCreators';
 
-class AddNewPay extends Component {
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.open}
-        toggle={this.props.toggle}
-        className={this.props.className}
+const AddNewPay = props => {
+  return (
+    <Modal
+      isOpen={props.open}
+      toggle={props.toggle}
+      className={props.className}
+    >
+      <Formik
+        initialValues={{
+          amount: 0,
+          date: format(new Date(), 'MM/DD/YYYY'),
+          tax: 0
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          props.onAddPay({ ...values, debt: props.debt }, setSubmitting);
+        }}
       >
-        <Formik
-          initialValues={{
-            amount: 0,
-            date: format(new Date(), 'MM/DD/YYYY'),
-            tax: 0
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            console.log(values);
-            this.props.onAddPay(
-              { ...values, debt: this.props.debt },
-              setSubmitting
-            );
-          }}
-        >
-          {({ handleChange, handleBlur, values }) => (
-            <Form>
-              <ModalHeader toggle={this.props.toggle}>
-                Agregar nuevo pago
-              </ModalHeader>
-              <ModalBody>
-                <div className="form-group">
-                  <label>Abono a capital</label>
-                  <Field className="form-control" name="amount" type="number" />
-                </div>
-                <div className="form-group">
-                  <label>Abono a interes</label>
-                  <Field className="form-control" name="tax" type="number" />
-                </div>
-                <div className="form-group">
-                  <label>Fecha del pago</label>
-                  <br />
-                  <DayPickerInput
-                    onDayChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.date}
-                    formatDate={this.formatDate}
-                    parseDate={this.parseDate}
-                    format={'MM/DD/YYYY'}
-                    className="form-control"
-                  />
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>{' '}
-                <button className="btn btn-danger" onClick={this.props.toggle}>
-                  Cancelar
-                </button>
-              </ModalFooter>
-            </Form>
-          )}
-        </Formik>
-      </Modal>
-    );
-  }
-}
-
-const mapStateToProps = state => ({});
+        {({ handleChange, handleBlur, values }) => (
+          <Form>
+            <ModalHeader toggle={props.toggle}>Agregar nuevo pago</ModalHeader>
+            <ModalBody>
+              <div className="form-group">
+                <label>Abono a capital</label>
+                <Field className="form-control" name="amount" type="number" />
+              </div>
+              <div className="form-group">
+                <label>Abono a interes</label>
+                <Field className="form-control" name="tax" type="number" />
+              </div>
+              <div className="form-group">
+                <label>Fecha del pago</label>
+                <br />
+                <DayPickerInput
+                  onDayChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.date}
+                  format={'MM/DD/YYYY'}
+                  className="form-control"
+                />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>{' '}
+              <button className="btn btn-danger" onClick={props.toggle}>
+                Cancelar
+              </button>
+            </ModalFooter>
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ onAddPay: initAddPay }, dispatch);
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(AddNewPay);
